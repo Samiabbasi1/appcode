e4c788595d179b391b152070968f3986150df4cd
Abdul Sami <120517327+Samiabbasi1@users.noreply.github.com>
2024-08-06 21:02:58 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
                         fontWeight: "bolder",
                     }}
                 >
-                    10WeeksofCloudOps - TODO LIST
+                    Sami's TODO List
                 </Row>
   
                 <hr />
